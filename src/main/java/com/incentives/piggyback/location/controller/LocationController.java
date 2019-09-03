886103dee80587e39245e2c89941e5caba53f538
Rajan Gupta <rajan0493@gmail.com>
2019-09-03 19:42:37 +0800

    Include GCP Pub/Sub Publisher

--- src/main/java/com/incentives/piggyback/location/controller/LocationController.java
+++ src/main/java/com/incentives/piggyback/location/controller/LocationController.java
@@ -1,11 +1,11 @@
 package com.incentives.piggyback.location.controller;
 
+import com.incentives.piggyback.location.publisher.LocationEventPublisher;
+import com.incentives.piggyback.location.utils.CommonUtility;
+import com.incentives.piggyback.location.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.incentives.piggyback.location.entity.Location;
 import com.incentives.piggyback.location.exception.PiggyException;
@@ -13,16 +13,35 @@ import com.incentives.piggyback.location.service.LocationService;
 import com.incentives.piggyback.location.utils.RestResponse;
 import com.incentives.piggyback.location.utils.RestUtils;
 
+import java.util.Calendar;
+import java.util.UUID;
+
 @RestController
 public class LocationController {
 
-	
 	@Autowired
 	private LocationService locationService;
-	
-	@RequestMapping(value="/location", method = RequestMethod.POST)
+	@Autowired
+	private LocationEventPublisher.PubsubOutboundGateway messagingGateway;
+
+	@RequestMapping(value="/location")
+	@PostMapping
 	public ResponseEntity<RestResponse<String>> saveLocationCoordinates(@RequestBody 
 			Location location) throws PiggyException {
-		return RestUtils.successResponse(locationService.saveLocationCoordinates(location));
+
+		ResponseEntity<RestResponse<String>> response =
+				RestUtils.successResponse(locationService.saveLocationCoordinates(location));
+
+		messagingGateway.sendToPubsub(
+				CommonUtility.stringifyEventForPublish(
+						UUID.randomUUID().toString(),
+						Constant.LOCATION_CREATED_EVENT,
+						Calendar.getInstance().getTime().toString(),
+						"",
+						Constant.LOCATION_SOURCE_ID
+						));
+
+		return response;
 	}
+
 }
