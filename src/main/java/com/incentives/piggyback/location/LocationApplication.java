3ce8263f952d7cc3a6ed9ba88ce8535abd5139c2
Rajan Gupta <rajan0493@gmail.com>
2019-11-02 18:59:12 +0800

    Jasypt encryption

--- src/main/java/com/incentives/piggyback/location/LocationApplication.java
+++ src/main/java/com/incentives/piggyback/location/LocationApplication.java
@@ -1,9 +1,11 @@
 package com.incentives.piggyback.location;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class LocationApplication {
 
 	public static void main(String[] args) {
